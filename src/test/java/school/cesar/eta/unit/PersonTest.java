a6f77ffdcd75a9f7c68ec110464457b4cbe5742b
Ariana <arianalima@github.com>
2021-02-20 20:45:45 -0300

    First tests

--- src/test/java/school/cesar/eta/unit/PersonTest.java
+++ src/test/java/school/cesar/eta/unit/PersonTest.java
@@ -1,10 +1,29 @@
 package school.cesar.eta.unit;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class PersonTest {
+    class PersonSpy extends Person{
+        int counter = 0;
+
+        @Override
+        public void addToFamily(Person person) {
+            super.addToFamily(person);
+            counter ++;
+        }
+    }
+
+    Person person;
+
+    @BeforeEach
+    public void setup(){
+        person = new Person();
+    }
+
     @Test
     public void getName_firstNameJonLastNameSnow_jonSnow() {
         fail();
@@ -42,12 +61,18 @@ public class PersonTest {
 
     @Test
     public void addToFamily_somePerson_familyHasNewMember() {
-        fail();
+        PersonSpy somePerson = new PersonSpy();
+        somePerson.addToFamily(this.person);
+        Assertions.assertEquals(1, somePerson.counter);
+        Assertions.assertTrue(somePerson.isFamily(this.person));
     }
 
     @Test
     public void addToFamily_somePerson_personAddedAlsoHasItsFamilyUpdated() {
-        fail();
+        PersonSpy somePerson = new PersonSpy();
+        somePerson.addToFamily(this.person);
+        Assertions.assertTrue(somePerson.isFamily(this.person));
+        Assertions.assertTrue(this.person.isFamily(somePerson));
     }
 
     @Test
