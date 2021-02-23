bbb1e0b0c7347069cf52badb9a12c77d9e209b38
Thamires Neves <thamires_neves@apple.com>
2021-02-23 19:05:35 -0300

    Project: Adding Person tests

--- src/test/java/school/cesar/eta/unit/PersonTest.java
+++ src/test/java/school/cesar/eta/unit/PersonTest.java
@@ -1,62 +1,139 @@
 package school.cesar.eta.unit;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class PersonTest {
+
+    @InjectMocks
+    Person person;
+
+    @Spy
+    List<Person> family = new ArrayList<Person>();
+
+    class SpyPerson extends Person {
+        private List<Person> family = new ArrayList<Person>();
+
+        @Override
+        public LocalDate getNow() {
+            return LocalDate.parse("2018-11-01");
+        }
+
+        @Override
+        public void addToFamily(Person person) {
+            this.family.add(person);
+        }
+    }
+
     @Test
     public void getName_firstNameJonLastNameSnow_jonSnow() {
-        fail();
+        person.setName("Jon");
+        person.setLastName("Snow");
+
+        String expected = "Jon Snow";
+        String actual = person.getName();
+
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void getName_firstNameJonNoLastName_jon() {
-        fail();
+        person.setName("Jon");
+
+        String expected = "Jon";
+        String actual = person.getName();
+
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void getName_noFirstNameLastNameSnow_snow() {
-        fail();
+        person.setLastName("Snow");
+
+        String expected = "Snow";
+        String actual = person.getName();
+
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void getName_noFirstNameNorLastName_throwsException() {
-        fail();
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            person.getName();
+        });
     }
 
     @Test
     public void isBirthdayToday_differentMonthAndDay_false() {
-        fail();
+        SpyPerson person = new SpyPerson();
+        LocalDate dt = LocalDate.parse("2018-12-02");
+
+        person.setBirthday(dt);
+        Assertions.assertFalse(person.isBirthdayToday());
     }
 
     @Test
     public void isBirthdayToday_sameMonthDifferentDay_false() {
-        fail();
+        SpyPerson person = new SpyPerson();
+        LocalDate dt = LocalDate.parse("2018-11-02");
+
+        person.setBirthday(dt);
+        Assertions.assertFalse(person.isBirthdayToday());
     }
 
     @Test
     public void isBirthdayToday_sameMonthAndDay_true() {
-        fail();
+        SpyPerson person = new SpyPerson();
+        LocalDate dt = LocalDate.parse("2018-11-01");
+
+        person.setBirthday(dt);
+        Assertions.assertTrue(person.isBirthdayToday());
     }
 
     @Test
     public void addToFamily_somePerson_familyHasNewMember() {
-        fail();
+        Person relative = new Person();
+        person.addToFamily(relative);
+
+        verify(family, times(1)).add(relative);
     }
 
     @Test
     public void addToFamily_somePerson_personAddedAlsoHasItsFamilyUpdated() {
-        fail();
-    }
+        Person relative = new Person();
+        relative.addToFamily(person);
 
-    @Test
-    public void isFamily_nonRelativePerson_false() {
-        fail();
+        verify(family, times(1)).add(relative);
     }
 
     @Test
     public void isFamily_relativePerson_true() {
-        fail();
+        Person relative = new Person();
+        relative.setName("Jon");
+
+        when(family.contains(relative)).thenReturn(true);
+        Assertions.assertTrue(person.isFamily(relative));
+    }
+
+    @Test
+    public void isFamily_nonRelativePerson_false() {
+        Person nonRelative = new Person();
+
+        nonRelative.setName("Joffrey");
+
+        boolean actual = person.isFamily(nonRelative);
+        assertFalse(actual);
     }
 }
